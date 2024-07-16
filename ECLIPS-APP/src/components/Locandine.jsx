33c6e769f124873ea39fbf364208f38e57363c76
iNeoxXIde <marco.iaquinandi@hotmail.com>
2024-07-16 20:17:53 +0200

    Aggiunto componente locandine, devono fixarsi alcune cose

---
+++ ECLIPS-APP/src/components/Locandine.jsx
@@ -0,0 +1,19 @@
+import "./Locandine.css";
+import React, { useState, useEffect } from "react";
+import Button from "./Button";
+
+function Locandine({ title, copertina }) {
+  return (
+    <div className="film-item">
+      <div className="image-container">
+        <img src={copertina} alt=""></img>
+      </div>
+      <div className="buttons-buy-container">
+        <Button text="ACQUISTA ORA" cssClass={"btn-buy"} />
+        <Button text="Vedi Trailer" cssClass={"btn-trailer"} />
+      </div>
+      <a className="title-film"> {title}</a>
+    </div>
+  );
+}
+export default Locandine;
--- ECLIPS-APP/src/components/Navbar.jsx
+++ ECLIPS-APP/src/components/Navbar.jsx
@@ -1,25 +1,40 @@
-import React, {  useState } from 'react';
-import './NavBar.css'; 
-import Button from './Button';
-
+import React, { useState } from "react";
+import "./Navbar.css";
+import Button from "./Button";
 
 function Navbar({ links }) {
+  const [showList, setShowList] = useState(false);
+
 
   return (
     <>
-    <nav className='container-navbar'>
-      <img className='logo-img' src='src/assets/img/Logo.png'></img>
-      <ul className="navbar-links">
-        {links.map((link, index) => (
-          <li className='linkNav' key={index}>
-            <a href={link.url}>{link.text}</a>
-          </li>
-        ))}
-      </ul>
-        <Button text="LOG IN" cssClass={"Button-LogIN show-list"}/>
-    </nav>
+      <nav className="container-navbar">
+        <img className="logo-img" src="src/assets/img/Logo.png"></img>
+        <ul className="navbar-links">
+          {links.map((link, index) => (
+            <li className="linkNav" key={index}>
+              <a href={link.url}>{link.text}</a>
+            </li>
+          ))}
+        </ul>
+        <div onMouseEnter={() => setShowList(true)} onMouseLeave={() => setShowList(false)} >
+          <Button text="LOG IN" cssClass={"Button-LogIN"} />
+          <div className="Container-LogIN-Menu"> 
+            {showList && (
+              <ul className="LogIN-Menu">
+                <li>
+                  <a href="#">Log In</a>
+                </li>
+                <li>
+                  <a href="#">Registrati</a>
+                </li>
+              </ul>
+            )}
+          </div>
+          
+        </div>
+      </nav>
     </>
-   
   );
-};
-export default Navbar
+}
+export default Navbar;
