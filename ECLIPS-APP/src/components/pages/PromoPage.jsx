db6358c4fbc297a29326f65fec91de47e8bb5056
antocilano <antocil1988@gmail.com>
2024-08-01 19:45:00 +0200

    bug fix

--- ECLIPS-APP/src/components/pages/PromoPage.jsx
+++ ECLIPS-APP/src/components/pages/PromoPage.jsx
@@ -6,12 +6,6 @@ import "../../index.css";
 function PromoPage () {
     return (
         <>
-        <Navbar  links={[
-          { url: "/", text: "Home" },
-          { url: "#", text: "Coming Soon" },
-          { url: "promo", text: "Promo" },
-          { url: "#", text: "Dove Trovarci" }
-        ]}/>
          <List title ="LISTINO PREZZI" image="../src/assets/img/immaginebiglietti.png" />
          <List title ="CONVENZIONI E SCONTI" image = "  ../src/assets/img/immaginepromo.png" />
          <List title ="ABBONAMENTI" image="../src/assets/img/tesseraabbonamento.png" />
