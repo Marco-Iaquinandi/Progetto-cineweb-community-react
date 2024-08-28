0a9cedacb575096d43781154ec16e4f960b900eb
iNeoxXIde <marco.iaquinandi@hotmail.com>
2024-08-28 20:48:11 +0200

    Form di login completo

--- ECLIPS-APP/src/App.jsx
+++ ECLIPS-APP/src/App.jsx
@@ -1,37 +1,55 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
+import PromoPage from "./components/pages/PromoPage";
+import Navbar from "./components/Navbar";
+import { PrimeReactProvider } from "primereact/api";
+import FormRegister from "./components/FormRegister";
+import FormLogIn from "./components/FormLogIn";
+import { UserProvider } from "./components/ContextLog";
 import "./index.css";
-import PromoPage from './components/pages/PromoPage';
-import Navbar from './components/Navbar';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import 'primeflex/primeflex.css';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import "primeflex/primeflex.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
-function App () {
-    return (
-       <PrimeReactProvider >
+function App() {
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const toggleLoginDialog = () => {
+    setShowLogin(!showLogin);
+  };
+  const toggleRegisterSidebar = () => {
+    setShowRegister(!showRegister);
+    setShowLogin(false);
+  };
+
+  return (
+    <UserProvider>
+      <PrimeReactProvider>
         <BrowserRouter>
-         <Navbar
-        links={[
-          { url: "/", text: "Home" },
-          { url: "#", text: "Coming Soon" },
-          { url: "promo", text: "Promo" },
-          { url: "#", text: "Dove Trovarci" },
-        ]}
-      ></Navbar>
-        <Routes>
-          <Route path='/' element= { <HomePage />} />
-          <Route path='/promo' element= { <PromoPage />} />
-        </Routes>
+          <Navbar
+            links={[
+              { url: "/", text: "Home" },
+              { url: "#", text: "Coming Soon" },
+              { url: "promo", text: "Promo" },
+              { url: "#", text: "Dove Trovarci" },
+            ]}
+          ></Navbar>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/promo" element={<PromoPage />} />
+            <Route path="/login" element={<FormLogIn isToggled={showLogin} onToggle={toggleLoginDialog} onRegisterClick={toggleRegisterSidebar} />} />
+          </Routes>
         </BrowserRouter>
-      </PrimeReactProvider>        
-    )
-};
+      </PrimeReactProvider>
+    </UserProvider>
+  );
+}
 
 export default App;
--- ECLIPS-APP/src/components/pages/HomePage.jsx
+++ ECLIPS-APP/src/components/pages/HomePage.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import Navbar from "../Navbar";
 import "../../index.css";
 import Locandine from "../Locandine";
 import filmsData from "../Listafilms.json";
 import AsideBar from "../AsideBar";
 /* import "./HomePage.css"; */
 function HomePage() {
-    const films = filmsData.films;
+  const films = filmsData.films;
   return (
     <>
       <div className="Locandine-container">
-      {films.map((film) => (
-        <Locandine 
-          key={film.idFilm}
-          title={film.titolo}
-          copertina={`../../src/assets/img/${film.copertina}`}
-        ></Locandine>
-      ))}
-      </div> 
-      <AsideBar />      
+        {films.map((film) => (
+          <Locandine
+            key={film.idFilm}
+            title={film.titolo}
+            copertina={`../../src/assets/img/${film.copertina}`}
+          ></Locandine>
+        ))}
+      </div>
+      <AsideBar />
     </>
   );
 }
