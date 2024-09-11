483bec122c39495a5a3df8dfb35a7c93254a8823
MirkoDGS <digesumirko@gmail.com>
2024-09-11 22:13:13 +0200

    sistemazione misure e media query

--- ECLIPS-APP/src/components/ScrollToTopButton.jsx
+++ ECLIPS-APP/src/components/ScrollToTopButton.jsx
@@ -1,40 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import './ScrollToTopButton.css'; 
+import React, { useState, useEffect } from "react";
+import "./ScrollToTopButton.css";
 
 function ScrollToTopButton() {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    
-    useEffect(() => {
-        const toggleVisibility = () => {
-            if (window.pageYOffset > 300) {
-                setIsVisible(true);
-            } else {
-                setIsVisible(false);
-            }
-        };
-
-        window.addEventListener('scroll', toggleVisibility);
-
-        return () => window.removeEventListener('scroll', toggleVisibility);
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth' // Scroll animato
-        });
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
     };
 
-    return (
-        <div>
-            {isVisible && (
-                <button onClick={scrollToTop} className="scroll-to-top">
-                    ↑ Torna su
-                </button>
-            )}
-        </div>
-    );
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      {isVisible && (
+        <button onClick={scrollToTop} className="scroll-to-top">
+          <i className="pi pi-chevron-up"></i>
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default ScrollToTopButton;
